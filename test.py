d1665198f5d1f934220e0fa9e36e590813fe2330
Leonardo0101 <leonardo@cantarella.it>
2023-03-09 23:25:43 +0100

    first testes

--- test.py
+++ test.py
@@ -1,6 +1,20 @@
 import os
 import telebot
+import requests
+import string
 
-BOT_TOKEN = os.environ.get('BOT_TOKEN')
+token = open('/home/leo/fcebot_token.txt','r')
+BOT_TOKEN = str(token.read())
 
-bot = telebot.TeleBot(BOT_TOKEN)
+bot = telebot.TeleBot(BOT_TOKEN.strip())
+
+def get_news(message):
+    #news_page=requests.get(url='https://www.circumetnea.it/category/news/')
+    #bot.reply_to(message,news_page.text)
+    bot.reply_to(message,'ciao')
+
+@bot.message_handler(commands=['news']) 
+def handle_news(message):
+    get_news(message)
+
+bot.infinity_polling()
