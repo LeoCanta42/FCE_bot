ff32b9088dbf77782a548cf81b57e0fc08e5c35f
Leonardo0101 <leonardo@cantarella.it>
2023-03-10 13:32:20 +0100

    starting testing real functions and buttons

--- test.py
+++ test.py
@@ -1,24 +1,65 @@
-import os
-import telebot
-import requests
-import string
+import logging
+from telegram import InlineKeyboardButton,InlineKeyboardMarkup,Update
+from telegram.ext import ApplicationBuilder,ContextTypes,CommandHandler,CallbackQueryHandler
 
-token = open('fcebot_token.txt','r')
-BOT_TOKEN = str(token.read())
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
-bot = telebot.TeleBot(BOT_TOKEN.strip())
+def general_markup():
+    keyboard = [
+        [
+            InlineKeyboardButton("Orario bus", callback_data="1"),
+            InlineKeyboardButton("Orario treni", callback_data="2"),
+        ],
+        [
+            InlineKeyboardButton("Orario metro", callback_data="3"),
+            InlineKeyboardButton("Controlla orario vicino", callback_data="4")]
+    ]
+    markup = InlineKeyboardMarkup(keyboard)
+    return markup
 
-def get_news(message):
-    #news_page=requests.get(url='https://www.circumetnea.it/category/news/')
-    #bot.reply_to(message,news_page.text)
-    bot.reply_to(message,'ciao')
+def add_handlers():
+    handlers = [
+        CommandHandler('start',welcome),
+        CommandHandler('help',help),
+        CallbackQueryHandler(button)
+        
+    ]
+    return handlers
 
-@bot.message_handler(commands=['news']) 
-def handle_news(message):
-    get_news(message)
+async def button(message: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = message.callback_query
 
-@bot.message_handler(commands=['start']) 
-def welcome(message):
-    bot.send_message(message.chat.id,'Benvenuto nel bot per info su Ferrovia Circumetnea')
+    await query.answer()
 
-bot.polling()
+    if(query.data == "1"): 
+        await context.bot.send_message(chat_id=message.effective_chat.id, text="Funziona1")
+    elif(query.data == "2"):
+        await context.bot.send_message(chat_id=message.effective_chat.id, text="Funziona2")
+    elif(query.data == "3"):
+        await context.bot.send_message(chat_id=message.effective_chat.id, text="Funziona3")
+    elif(query.data == "4"):
+        await context.bot.send_message(chat_id=message.effective_chat.id, text="Funziona4")
+
+async def welcome(message: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=message.effective_chat.id, text="Benvenuto nel bot per info sulla Ferrovia Circumetnea",reply_markup=general_markup())
+
+async def help(message: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=message.effective_chat.id, text=
+"""Orario bus: scarica pdf orario bus
+Orario treni: scarica pdf orario treni
+Orario metro: scarica pdf orario metro 
+Controlla orario vicino: calcola l'orario piu' vicino a quello desiderato per andare da/a """)
+
+def main():
+    token = str(open('fcebot_token.txt','r').read()).strip()
+    bot= ApplicationBuilder().token(token).build()
+
+    bot.add_handlers(add_handlers())
+    
+    bot.run_polling()
+
+if __name__ == '__main__':
+    main()
