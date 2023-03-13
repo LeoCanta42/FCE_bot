26a984bb7d9be234a038b2661bc31e182c737925
Leonardo0101 <leonardo@cantarella.it>
2023-03-13 16:13:08 +0100

    understanding use of tabula to work with rows and columns of pdf table (timetables for lines and times)

--- module/timetables_operations/calculate_times.py
+++ module/timetables_operations/calculate_times.py
@@ -1,15 +1,24 @@
 import tabula
 
-def define_locations(s:str): 
-    table=tabula.read_pdf(s, pages='all', lattice=True)[0] #prima tabella #pandas_options={'header':None}
-    first_col=table.iloc[1:,:1] #iloc[row:row,col:col]
-    for i in first_col.values:
-        print(i)
-    
-    print("SECONDA \n")
-    table=tabula.read_pdf(s, pages='all', lattice=True)[1] #seconda tabella #pandas_options={'header':None}
-    first_col=table.iloc[1:,:1] #iloc[row:row,col:col]
-    for i in first_col.values:
-        print(i)
+file="./module/timetables_operations/locations.txt"
+pdf="./module/timetables_operations/bus_orario0.pdf"
 
-define_locations("./bus_orario0.pdf")
+
+def extract_locations_onTXT(s:str):
+    with open(file,"w") as f:
+        f.write("")
+    table=tabula.read_pdf(s, pages='all', stream=True)[0] #prima tabella #pandas_options={'header':None}    
+    rows=table.shape[0]  
+    #cols=table.shape[1]
+    with open(file,"a") as f:
+        for i in range(rows):
+            row=table.iloc[i:i+1,:].values #prendo la prima riga della tabella
+            for j in row[0][:]: #[0] perche' vede anche una riga come una matrice di una sola riga
+                f.write(str(j)+" | ") # scrivo ogni elemento della riga (scorrendo le colonne) e mettendo il separatore |
+            f.write(" \n")
+    
+#extract_locations_onTXT(pdf)
+
+
+def find_lines(s:str,a:str,b:str):
+    
