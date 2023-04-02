f3fac1dc2b7024977ee1de4cdfec46f4bd635a37
Leonardo0101 <leonardo@cantarella.it>
2023-04-02 16:27:49 +0200

    adding a comment for explaining how to get channel_chat_id

--- bot_news_sender.py
+++ bot_news_sender.py
@@ -7,6 +7,10 @@ import asyncio
 token=str(open("news_fcebot_token.txt","r").read()).strip()
 bot=Bot(token=token)
 
+#per ottenere chat id
+#https://api.telegram.org/bot'bottoken'/sendMessage?chat_id=@'channel_name_public'&text=123
+#nella risposta e' contenuto il chat id del canale, una volta preso si puo' reimpostare il canale a privato
+
 channel_chat_id=str(open("./module/news_check/channel_id.txt","r").read()).strip()
 
 def sender(bot:Bot,chat):
