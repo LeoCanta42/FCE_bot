56f77ea1ec3615e6331aafd5cb1cac174fc64dd6
Leonardo0101 <leonardo@cantarella.it>
2023-04-05 17:01:59 +0200

    fixing another problem on times_op for recognising even H:M times

--- module/timetables_operations/times_op.py
+++ module/timetables_operations/times_op.py
@@ -2,10 +2,14 @@ from datetime import datetime
 
 def isTimeFormat(input:str) -> bool:
     try:
-        datetime.strptime(input, "%H.%M")
+        if input.find(":")>=0:
+            datetime.strptime(input, "%H:%M")
+        else:    
+            datetime.strptime(input, "%H.%M")
         return True
     except:
         return False
+    
 def isTimeFormatH(input:str) -> bool:
     try:
         datetime.strptime(input, "%H")
@@ -14,12 +18,20 @@ def isTimeFormatH(input:str) -> bool:
         return False
 
 def format_time(t:str) -> str:
-    splitted_time=t.split('.')
+    if t.find(":")>=0:
+        separator=':'
+    else:
+        separator='.'
+    splitted_time=t.split(separator)
     #es: 8.5 viene interpretato come 08.05 quindi  mi separo in [8,5]
     # se il secondo e' di lunghezza 1 aggiungo 0 per fare 50 e poi unisco
-    if len(splitted_time)==2 and len(splitted_time[1])==1: #lo faccio solo se ho H.M, se ho solo H non mi serve
-        splitted_time[1]+='0'
+    if len(splitted_time)==2:
+        if len(splitted_time[1])==1: #lo faccio solo se ho H.M, se ho solo H non mi serve
+            splitted_time[1]+='0'
         t=splitted_time[0]+'.'+splitted_time[1]
+    elif len(splitted_time)==1:
+        t=splitted_time[0]
+
     if isTimeFormat(t):
         tmp=datetime.strptime(t,"%H.%M")
     elif isTimeFormatH(t):
