4dc55a8278799d0160699d256d34d344c9c1b91b
Leonardo0101 <leonardo@cantarella.it>
2023-04-05 12:39:54 +0200

    fixing another error on times_op for checking if just Hour

--- module/timetables_operations/times_op.py
+++ module/timetables_operations/times_op.py
@@ -17,9 +17,9 @@ def format_time(t:str):
     splitted_time=t.split('.')
     #es: 8.5 viene interpretato come 08.05 quindi  mi separo in [8,5]
     # se il secondo e' di lunghezza 1 aggiungo 0 per fare 50 e poi unisco
-    if len(splitted_time[1])==1: 
+    if len(splitted_time)==2 and len(splitted_time[1])==1: #lo faccio solo se ho H.M, se ho solo H non mi serve
         splitted_time[1]+='0'
-    t=splitted_time[0]+'.'+splitted_time[1]
+        t=splitted_time[0]+'.'+splitted_time[1]
     if isTimeFormat(t):
         tmp=datetime.strptime(t,"%H.%M")
     elif isTimeFormatH(t):
