8d7dd753a3e0e1a42c70183602f48ad7411c2c0a
Leonardo0101 <leonardo@cantarella.it>
2023-04-27 08:44:46 +0200

    creating a script for sending messages to users, in case of new feature or problems

--- scheduler.sh
+++ scheduler.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 #source .venv/bin/activate
 h=true
-t=true
-
-python3.10 bot_news_sender.py & #bot notizie
 
 while true; do
-	if $h; then
-		kill $PID
-		python3.10 mainbot.py & #bot principale
-		PID=$!
-		h=false #se e' gia partito non deve ripartire
-	fi
-	if [ "$(date '+%H:%M')" = "01:00" ]; then #all'1:00 effettuo check di nuovi orari	
-		kill $PID
-		python3.10 bot_pause_messages.py & #bot quando bot in pausa
-		PID=$!
-		python3.10 autocheck.py
-		h=true #faccio ripartire il bot
-	fi
-	sleep 58 #<1 min in modo da beccare sempre
+        if $h; then
+                kill $PID1
+                python3.9 mainbot.py & #bot principale
+                PID1=$!
+                python3.9 bot_news_sender.py & #bot notizie
+                PID2=$!
+                h=false #se e' gia partito non deve ripartire
+        fi
+        if [ "$(date '+%H:%M')" = "01:00" ]; then #all'1:00 effettuo check di n>
+                kill $PID1
+                kill $PID2
+                python3.9 bot_pause_messages.py & #bot quando bot in pausa
+                PID1=$!
+                python3.9 autocheck.py
+                h=true #faccio ripartire il bot
+        fi
+        sleep 58 #<1 min in modo da beccare sempre
 done
+
---
+++ sending_messages_to_users.py
@@ -0,0 +1,16 @@
+from telegram import Bot
+import asyncio
+import sqlite3 as sql
+
+token=str(open("./module/private/fcebot_token.txt","r").read()).strip()
+bot=Bot(token=token)
+tosend='''
+
+'''
+
+with sql.connect("users.db") as connection:
+    cursor=connection.cursor()
+    chat_ids=cursor.execute("select chatid from Users").fetchall()
+
+for chat in chat_ids:
+    asyncio.run(bot.send_message(chat_id=chat[0],text=tosend,parse_mode='Markdown')) 
