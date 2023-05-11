00c88d2433d78ec8ad0989c567156c8e161ea489
Leonardo0101 <leonardo@cantarella.it>
2023-05-11 17:11:54 +0200

    chore: modified scheduler to reboot host at 2 and modified mainbot to restart bot automatically in case of exceptions

--- mainbot.py
+++ mainbot.py
@@ -1,7 +1,13 @@
 from module.handlers_basic_commands import add_handlers
 from telegram.ext import ApplicationBuilder
-#from module.timetables_operations.extract_excel import load #NO DB#
+from module.timetables_operations.extract_excel import locations_to_file
+from module.timetables_operations.extract_excel import load #NO DB#
+from time import sleep
+
 #from module.timetables_operations.calculate_times import find_lines2 #see all lines| just for debug#
+
+import os
+# os.chdir("/home/pi/FCE_bot")
 
 token = str(open('./module/private/fcebot_token.txt','r').read()).strip()
 app= ApplicationBuilder().token(token).build()
@@ -11,7 +17,12 @@ def mainbot() -> None :
     #togliere commenti con #NO DB# per usare old find lines (no DB), anche da handlers_basic_commands
     #load("bus") #NO DB#
     #load("littorina") #NO DB#
-    app.run_polling()
+    try:
+        app.run_polling()
+    except Exception as e:
+        print(e)
+        sleep(60)
+        os.system('bash /home/leo/Desktop/FCE_bot/restarter.sh restart')
 
 if __name__ == '__main__':
     mainbot()
--- scheduler.sh
+++ scheduler.sh
@@ -5,19 +5,22 @@ h=true
 while true; do
         if $h; then
                 kill $PID1
-                python3.9 mainbot.py & #bot principale
+                python3.10 mainbot.py & #bot principale
                 PID1=$!
-                python3.9 bot_news_sender.py & #bot notizie
+                python3.10 bot_news_sender.py & #bot notizie
                 PID2=$!
                 h=false #se e' gia partito non deve ripartire
         fi
         if [ "$(date '+%H:%M')" = "01:00" ]; then #all'1:00 effettuo check di n>
                 kill $PID1
                 kill $PID2
-                python3.9 bot_pause_messages.py & #bot quando bot in pausa
+                python3.10 bot_pause_messages.py & #bot quando bot in pausa
                 PID1=$!
-                python3.9 autocheck.py
+                python3.10 autocheck.py
                 h=true #faccio ripartire il bot
+        fi
+        if [ "$(date '+%H:%M')" = "02:00" ]; then #alle 2 riavvio
+                sudo reboot
         fi
         sleep 58 #<1 min in modo da beccare sempre
 done
