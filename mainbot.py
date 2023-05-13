d3ba3918193bf6f0698668968976703ebb357d6e
Leonardo0101 <leonardo@cantarella.it>
2023-05-13 12:37:13 +0200

    chore: modified try,except to avoid not starting bot

--- mainbot.py
+++ mainbot.py
@@ -1,7 +1,6 @@
 from module.handlers_basic_commands import add_handlers
 from telegram.ext import ApplicationBuilder
-from module.timetables_operations.extract_excel import locations_to_file
-from module.timetables_operations.extract_excel import load #NO DB#
+
 from time import sleep
 
 #from module.timetables_operations.calculate_times import find_lines2 #see all lines| just for debug#
@@ -14,16 +13,12 @@ app= ApplicationBuilder().token(token).build()
 app.add_handlers(add_handlers()) 
 
 def mainbot() -> None : 
-    #togliere commenti con #NO DB# per usare old find lines (no DB), anche da handlers_basic_commands
-    #load("bus") #NO DB#
-    #load("littorina") #NO DB#
     try:
         app.run_polling()
     except Exception as e:
         print(e)
-        sleep(60)
-        os.system('bash /home/leo/Desktop/FCE_bot/restarter.sh restart')
+        sleep(30)
+        os.system('bash /home/leo/Desktop/FCE_bot/restarter.sh restart &')
 
 if __name__ == '__main__':
     mainbot()
-    #find_lines2()
